05b211aca8d45dd9d2be7d776f2ba42d646f572a
Victor Ivanchenko <ivanvikvik@gmail.com>
2024-02-26 14:02:46 +0200

    Update task02.cpp

--- StudentCppRevisionTask03Project/task02.cpp
+++ StudentCppRevisionTask03Project/task02.cpp
@@ -40,15 +40,11 @@
 *	[ input 8]: 1 15 -2
 *	[output 8]: error
 *
-*	[ input 9]: 1 15 -2
+*	[ input 9]: 0 0 2
 *	[output 9]: error
 *
-*	[ input 10]: 0 0 2
+*	[ input 10]: 1 50 0
 *	[output 10]: error
-*
-*	[ input 11]: 1 50 0
-*	[output 11]: error
-*
 */
 
 string task02(int a, int b, int n) {
